4aa99ac870b7078a01465ae06125ab4ee20dcc56
Cameron Dershem <cameron@pinkhatbeard.com>
2021-03-19 15:26:21 -0400

    Fixes something in the tests.

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -8,7 +8,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Act
     let response = app.post_subscriptions(body.into()).await;
-    dbg!(&response);
 
     // Assert
     assert_eq!(200, response.status().as_u16());
@@ -24,6 +23,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
 #[actix_rt::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
     let app = spawn_app().await;
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
@@ -32,12 +32,15 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     ];
 
     for (invalid_body, error_message) in test_cases {
+        // Act
         let response = app.post_subscriptions(invalid_body.into()).await;
 
+        // Assert
         assert_eq!(
             400,
             response.status().as_u16(),
-            "The API did not fail with 400 Bad Request when payload was {}.",
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
         );
     }
@@ -45,6 +48,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
 #[actix_rt::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    // Arrange
     let app = spawn_app().await;
     let test_cases = vec![
         ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
@@ -53,6 +57,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     ];
 
     for (body, description) in test_cases {
+        // Act
         let response = app.post_subscriptions(body.into()).await;
 
         // Assert

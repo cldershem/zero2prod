ef77a1ac0e56f36560c01802ef09112937358727
Cameron Dershem <cameron@pinkhatbeard.com>
2022-01-12 02:13:38 -0500

    Updates dependencies to match blog.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,3 +1,4 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
@@ -5,13 +6,17 @@ use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-lazy_static::lazy_static! {
-    static ref TRACING: () = {
-        let filter = if std::env::var("TEST_LOG").is_ok() { "debug" } else { "" };
-        let subscriber = get_subscriber("test".into(), filter.into());
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
         init_subscriber(subscriber);
     };
-}
+});
 
 pub struct ConfirmationLinks {
     pub html: reqwest::Url,
@@ -60,7 +65,7 @@ impl TestApp {
 }
 
 pub async fn spawn_app() -> TestApp {
-    lazy_static::initialize(&TRACING);
+    Lazy::force(&TRACING);
 
     let email_server = MockServer::start().await;
 
